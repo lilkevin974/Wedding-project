c084ce92eafbcca3a5612e969963c92d0bc542d4
KEV <lilkevin@hotmail.fr>
2020-08-30 22:30:09 +0200

    Added confirmation model

--- wedding/main/admin.py
+++ wedding/main/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from main.models import Confirmation
 
 # Register your models here.
+admin.site.register(Confirmation)
--- wedding/main/models.py
+++ wedding/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
 # Create your models here.
+
+class Confirmation(models.Model):
+    email=models.CharField(max_length=50)
+    last_name=models.CharField(max_length=20)
+    first_name=models.CharField(max_length=20)
+    adults=models.IntegerField()
+    children=models.IntegerField()
+    message=models.CharField(max_length=200)

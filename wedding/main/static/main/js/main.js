3f476233c8e8c5ece05e7e25d3c4c16592b7eecd
KEV <lilkevin@hotmail.fr>
2020-09-06 23:08:37 +0200

    Slider's style completed

--- wedding/main/static/main/js/main.js
+++ wedding/main/static/main/js/main.js
@@ -5,15 +5,20 @@ function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
 function showSlides(n) {
-    let i;
     const slide_image = document.querySelector('.slide-image');
-    const images = slide_image.children;
-    const slide_info = document.querySelector('.slide-info')
-    const info = slide_info.getElementsByTagName('span')
-   /*  const dots = document.getElementsByClassName("dot"); */
+    const images      = slide_image.children;
+    const slide_info  = document.querySelector('.slide-info');
+    const info        = slide_info.getElementsByTagName('span');
+    const dots        = document.getElementsByClassName("dot");
+    
     if (n > images.length) {slideIndex = 1}
     if (n < 1) {slideIndex = images.length}
+
     for (i = 0; i < images.length; i++) {
         images[i].classList.remove('opaque');
     }
@@ -26,31 +31,70 @@ function showSlides(n) {
         else{
             info[i].classList.remove('active')
         }
-
     }
 
-    /* for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    } */
+    for (i = 0; i < dots.length; i++) {
+        dots[i].style.backgroundColor='#bbb';
+    }
     selected_image.classList.add('opaque');
-    /* dots[slideIndex-1].className += "active"; */
+    dots[slideIndex-1].style.backgroundColor='#717171';
+
+    showInfo()
+    slideColor(selected_image.dataset.place)
 } 
   
 function showMap() {
-    const active          = document.querySelector('active');
+    const active          = document.querySelector('.active');
     const mapContainer    = document.querySelector('.map-container');
     const maps            = mapContainer.children;
+    const info            = document.querySelector('.info-reveal');
     const mairiePosition  = {lat: -20.879416, lng: 55.447837};
     const mairieContainer = document.getElementById('map-mairie');
+  
+    info.classList.remove('info-reveal')
     for (i=0; i < maps.length; i++){
         if (active.dataset.show == maps[i].dataset.show){
-            selectedMap=maps[i]
-        }
-        else{
-            maps[i].classList.remove('activeMap')
+            if (active.dataset.show == 'mairie'){
+                initMap(mairiePosition,mairieContainer)
+            }
         }
     }
-    selectedMap.classList.add('activeMap')
+    mapContainer.classList.add('info-reveal')
+}
+
+function showInfo() {
+    const mapContainer = document.querySelector('.map-container');
+    const info         = document.querySelector('.steps');
+    
+    mapContainer.classList.remove('info-reveal');
+    info.classList.add('info-reveal');
+}
+
+function slideColor(slide){
+    let color
+    let name
+    const label   = document.querySelector('.slide-label');
+    const showBut = document.querySelectorAll('.show');
+    const textBut = document.querySelector('.text');
+    const mapBut  = document.querySelector('.map')
+    const icons   = document.querySelectorAll('.material-icons-round');
+    if (slide == 'mairie'){color = '#f1faf9'; name  = 'Mairie';}
+    else if (slide == 'eglise'){color = '#faeeeb'; name  = 'Eglise';}
+
+    label.innerHTML=name;
+    label.style.backgroundColor=color;
+    
+    textBut.style.backgroundColor=color;
+    mapBut.style.backgroundColor='white';
+    for (i=0; i<showBut.length; i++){
+        showBut[i].addEventListener('click', (e)=> {
+            showBut.forEach(element => element.style.backgroundColor='white')
+            e.target.style.backgroundColor=color;
+            console.log(e.target)
+        })
+    }
+    icons.forEach(icon => icon.style.color=color)
+
 }
 
 
@@ -111,7 +155,16 @@ function initMap(position,container) {
     var mairie = {lat: -20.879416, lng: 55.447837};
     // The map, centered at Uluru
     var map = new google.maps.Map(
-        document.getElementById('map-mairie'), {zoom: 15, center: mairie});
+        document.getElementById('map-mairie'), {
+            mapId: '17a2994719f5f1cc',
+            zoom: 15,
+            center: mairie,
+            disableDefaultUI: true,
+            zoomControl: true,
+            scaleControl: true,
+            rotateControl: true,
+            fullscreenControl: true
+        });
     // The marker, positioned at Uluru
     var marker = new google.maps.Marker({position: mairie, map: map});
   }

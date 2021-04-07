4a0e9e864f98f0cf99f8223990f9a211aeb76d6b
KEV <lilkevin@hotmail.fr>
2021-04-08 00:37:58 +0200

    Sms test v4

--- wedding/main/admin.py
+++ wedding/main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from main.models import Confirmation
+from main.models import Confirmation, TwilioSms
 
 # Register your models here.
-admin.site.register(Confirmation)
+admin.site.register(Confirmation, TwilioSms)

c91b246ee14ae4b36b26375fe7348f3719a597f3
KEV <lilkevin@hotmail.fr>
2021-04-08 00:25:49 +0200

    Sms test

--- wedding/main/models.py
+++ wedding/main/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from twilio.rest import Client
+# Create your models here.
+
+
+env = environ.Env()
+environ.Env.read_env()
 # Create your models here.
 
 class Confirmation(models.Model):
@@ -8,3 +14,24 @@ class Confirmation(models.Model):
     adults=models.IntegerField()
     children=models.IntegerField()
     message=models.CharField(max_length=200)
+
+class TwilioSms(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+    category = models.CharField(max_length=30)
+    number = models.CharField(max_length=30)
+
+    def save(self, *args, **kwargs):
+
+        account_sid = env('TWILIO_ACCOUNT_SID')
+        auth_token = env('TWILIO_AUTH_TOKEN')
+
+        client = Client(account_sid, auth_token)
+
+        client.api.account.messages.create(
+            to=f"{self.number}",
+            from_="+14153197987",
+            body="Hello there!")
+
+        super().save(*args, **kwargs)

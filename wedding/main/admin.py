8b7f355335bc5fc184ab6711fbfd1d32cb60fea1
KEV <lilkevin@hotmail.fr>
2021-04-09 15:23:09 +0200

    edited admin panel v2

--- wedding/main/admin.py
+++ wedding/main/admin.py
@@ -8,6 +8,7 @@ admin.site.register(Confirmation)
 class SmsAdmin(admin.ModelAdmin):
     list_display('fullname', 'surname')
     list_editable('number','category')
+    list_filter('category')
 
     def fullname(self,obj):
         return "{} {}".format(obj.last_name, obj.first_name)

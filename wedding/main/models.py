ac5d8c36cec83cb776ebc20b592bca43068267ca
KEV <lilkevin@hotmail.fr>
2021-04-10 11:41:36 +0200

    Fixed model and confirmation message

--- wedding/main/models.py
+++ wedding/main/models.py
@@ -54,4 +54,4 @@ class Confirmation(models.Model):
     first_name=models.CharField(max_length=20)
     adults=models.IntegerField()
     children=models.IntegerField()
-    message=models.CharField(max_length=200)
+    message=models.CharField(max_length=200, blank=True)

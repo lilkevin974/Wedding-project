8bc15f60eaa70af1dc7df2ef0b28d5f11e7d570f
KEV <lilkevin@hotmail.fr>
2021-05-02 14:06:31 +0200

    Added favicon v3

--- wedding/main/urls.py
+++ wedding/main/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
-from django.contrib.staticfiles.storage import staticfiles_storage
-from django.views.generic.base import RedirectView
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('status',views.smsstatus, name='SmsStatus'),
-    path("favicon.ico",RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),),
+    path('status',views.smsstatus, name='SmsStatus')
 ]
